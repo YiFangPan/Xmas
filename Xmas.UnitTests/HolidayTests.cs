6e59b27500e1149a4cccdc256118b03c78a21a11
YiFangPan <sber13@gmail.com>
2020-05-07 23:49:10 +0800

    Add Xmas Test Method, extract method and generate interface for create fake object

--- Xmas.UnitTests/HolidayTests.cs
+++ Xmas.UnitTests/HolidayTests.cs
@@ -13,11 +13,37 @@ namespace Xmas.UnitTests
         [Test]
         public void Get_Today_Is_Not_Xmas_Value()
         {
-            Holiday holiday = new Holiday();
+            IDateTimeWrapper wrapper = new FakeDateTimeWrapper();
+            
+            var holiday = new Holiday(wrapper);
             var result = holiday.GetXmasValue();
             Assert.AreEqual("Today is not Xmas", result);
         }
+
+        private class FakeDateTimeWrapper : IDateTimeWrapper
+        {
+            public DateTime GetNow()
+            {
+                return new DateTime(2020, 12, 1);
+            }
+        }
+
         
+        [Test]
+        public void Get_Today_Is_Xmas_Value()
+        {
+            IDateTimeWrapper wrapper = new FakeXmasDateTimeWrapper();
+            var holiday = new Holiday(wrapper);
+            var result = holiday.GetXmasValue();
+            Assert.AreEqual("Xmas", result);
+        }
         
+        private class FakeXmasDateTimeWrapper : IDateTimeWrapper
+        {
+            public DateTime GetNow()
+            {
+                return new DateTime(2020, 12, 25);
+            }
+        }
     }
 }
--- Xmas/Holiday.cs
+++ Xmas/Holiday.cs
@@ -4,12 +4,36 @@ namespace Xmas
 {
     public class Holiday
     {
+        private IDateTimeWrapper _wrapper;
+        
+        public Holiday(IDateTimeWrapper wrapper)
+        {
+            _wrapper = wrapper;
+        }
+        
         public string GetXmasValue()
         {
-            DateTime today = DateTime.Today;
+            
+            DateTime today = _wrapper.GetNow();
             if (today.Month == 12 && today.Day == 25)
                 return "Xmas";
             return "Today is not Xmas";
+        }
+        
+        
+        
+    }
+
+    public interface IDateTimeWrapper
+    {
+        DateTime GetNow();
+    }
+    
+    public class DateTimeWrapper : IDateTimeWrapper
+    {
+        public DateTime GetNow()
+        {
+            return DateTime.Now;
         }
     }
 }
